e254b5ce883d4aa4820a2ba5e1c174bc642b2b8d
vladimirptomsk <vladimirptomsk@gmail.com>
2017-11-30 21:05:16 +0700

    Added a domain module

--- domain/src/main/java/com/mystudy/hotelservice/repository/CityRepository.java
+++ domain/src/main/java/com/mystudy/hotelservice/repository/CityRepository.java
@@ -2,6 +2,9 @@ package com.mystudy.hotelservice.repository;
 
 import com.mystudy.hotelservice.model.City;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CityRepository extends JpaRepository<City, Long> {
+
 }
--- src/main/java/com/mystudy/hotelservice/web/MainController.java
+++ src/main/java/com/mystudy/hotelservice/web/MainController.java
@@ -1,9 +1,22 @@
 package com.mystudy.hotelservice.web;
 
+import com.mystudy.hotelservice.model.City;
+import com.mystudy.hotelservice.repository.CityRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-//@RestController
+import java.util.List;
+
+@RestController
 public class MainController  {
 
+    @Autowired
+    CityRepository cityRepository;
 
+    @RequestMapping("/api/cities")
+    public @ResponseBody List<City> index(){
+        return (List<City>) cityRepository.findAll();
+    }
 }
