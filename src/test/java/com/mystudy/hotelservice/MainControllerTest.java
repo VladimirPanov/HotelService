23cb9b840e3cadff933c8a9e0617c7d43c9d3dd5
vladimir <vladimirptosmk@gmail.com>
2017-12-13 00:41:07 +0700

    Moved tests into main module

--- src/test/java/com/mystudy/hotelservice/MainControllerTest.java
+++ src/test/java/com/mystudy/hotelservice/MainControllerTest.java
@@ -2,9 +2,12 @@ package com.mystudy.hotelservice;
 
 import com.mystudy.hotelservice.model.City;
 import com.mystudy.hotelservice.model.Hotel;
+import com.mystudy.hotelservice.repository.CityRepository;
+import com.mystudy.hotelservice.repository.HotelRepository;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -12,16 +15,27 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class MainControllerTest {
 
+    @Autowired
+    CityRepository cityRepository;
+
+    @Autowired
+    HotelRepository hotelRepository;
+
     @Before
     public void setup(){
         City city = new City();
         city.setName("Москва");
+        cityRepository.save(city);
 
         Hotel hotel = new Hotel();
         hotel.setName("Центральная");
         hotel.setAddress("Пр. Мира, 10");
+        hotel.setCity(city);
+        hotelRepository.save(hotel);
     }
 
-    //@Test
+    @Test
+    public void contextLoads() {
+    }
 
 }
